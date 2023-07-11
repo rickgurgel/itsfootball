ad5b0c01b9c690ff175857bc244ebf0b17439ca9
Ricardo Gurgel <ricardo.gurgel@eletraenergy.com>
2023-07-11 19:26:49 -0300

    projeto criado

---
+++ src/main/java/com/itsfootball/itsfootball/ItsfootballApplication.java
@@ -0,0 +1,13 @@
+package com.itsfootball.itsfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsfootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsfootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsfootball/itsfootball/ItsfootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsfootball.itsfootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsfootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
