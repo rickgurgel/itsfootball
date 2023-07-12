99f580f86041e2fb5d3ff31871d82b0d4e571ef8
Ricardo Gurgel <rgurgelf@gmail.com>
2023-07-12 00:54:11 -0300

    domain model

--- src/main/java/com/itsfootball/itsfootball/entities/Game.java
+++ src/main/java/com/itsfootball/itsfootball/entities/Game.java
@@ -36,7 +36,6 @@ public class Game {
 
 	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
